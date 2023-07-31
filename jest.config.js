dcd4ee905938a07af4531c3efeebf1ab76776ca3
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-07-31 16:01:44 -0300

    chore: initial configuration

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const config = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverage: false,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  preset: '@shelf/jest-mongodb'
+}
+
+module.exports = config
