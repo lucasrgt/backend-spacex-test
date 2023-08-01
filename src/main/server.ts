3dafab35251e56670369113a980b8b84ffba0fa4
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-07-31 21:08:03 -0300

    chore: add dotenv

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,6 @@
 import app from './config/app'
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 6000
 
 app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
