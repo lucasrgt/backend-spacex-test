e871a1837c2ec50721b2b71575a5d63ee6008353
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-08-01 00:44:39 -0300

    feat: create LaunchController and http helpers

---
+++ src/presentation/controllers/launch/launch.ts
@@ -0,0 +1,40 @@
+import { AddLaunch } from '../../../domain/usecases/add-launch/add-launch-protocol'
+import { MissingParamError, InvalidParamError } from '../../errors'
+import { badRequest, ok, serverError } from '../../helpers/http-helpers'
+import { Controller, HttpRequest, HttpResponse } from '../../protocols'
+
+export class LaunchController implements Controller {
+  private readonly addLaunch: AddLaunch
+
+  constructor(addLaunch: AddLaunch) {
+    this.addLaunch = addLaunch
+  }
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const requiredFields = [
+        'flightNumber',
+        'name',
+        'dateUtc',
+        'dateUnix',
+        'dateLocal',
+        'datePrecision',
+        'upcoming'
+      ]
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      const { ...data } = httpRequest.body
+
+      const launch = await this.addLaunch.add(data)
+
+      return ok(launch)
+    } catch (error) {
+      console.error(error)
+      return serverError()
+    }
+  }
+}
